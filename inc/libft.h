60b350c67792e2cdbb5c16d5f62ba150f739e997
Caya <cavan-vl@student.codam.com>
2024-04-18 17:43:59 +0200

    W

--- inc/libft.h
+++ inc/libft.h
@@ -62,7 +62,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-
+void	*ft_malloc(size_t size);
 /*===============================OUTPUT================================*/
 
 int		ft_putchar_fd(char c, int fd);
